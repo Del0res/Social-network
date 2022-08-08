d3d9fec5d5a1a1043bbe62b34f1977e9d3d4113c
Kate Podus <katerinapodus@gmail.com>
2022-08-08 14:55:28 +0300

    Initial project settings

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+   return (
+      <header className='header'>
+         <img src="https://cdn.dribbble.com/userupload/3000718/file/original-3aad87878399c27752ecdb2deb03dcb0.jpg?resize=400x0" />
+      </header>
+   );
+}
+
+export default Header;
---
+++ src/Components/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+   return (
+      <div className='content'>
+         <div>
+            <img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" />
+         </div>
+         <div>
+            <img src="https://thumbs.dreamstime.com/b/young-man-standing-cliff-edge-celebrates-reaching-top-mountain-arms-air-shouting-young-man-cliff-edge-213584552.jpg" />
+         </div>
+         <div>
+            My posts
+            <div>
+               New post
+            </div>
+            <div>
+               <div>
+                  post 1
+               </div>
+               <div>
+                  post 2
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Profile;
